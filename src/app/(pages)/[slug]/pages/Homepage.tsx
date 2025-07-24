84c9c53129c9b1bd72da3b0e0abfd019395b0c33
3mpty <aurelien.corre@outlook.fr>
2025-07-24 22:32:46 +0200

    Removed unused declarations

--- src/app/(pages)/[slug]/pages/Homepage.tsx
+++ src/app/(pages)/[slug]/pages/Homepage.tsx
@@ -1,16 +1,4 @@
-// import { fetchGraphQL } from '@/lib/strapi/fetchGraphql';
-
-// const homeQuery = /* GRAPHQL ;) */ ``;
-
 export default async function Homepage() {
-  // const data = await fetchGraphQL<{
-  //   home: {
-  //     title: string;
-  //     description: string;
-  //     // ETC
-  //   };
-  // }>(homeQuery, {});
-
   return (
     <>
       <p>TEST TECHNIQUE</p>
